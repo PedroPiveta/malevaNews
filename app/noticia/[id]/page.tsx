ded195c6fb3c4069103c54ad3db3a857c1a31c39
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-07-22 17:43:07 -0300

    added dynamic route

---
+++ app/noticia/[id]/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/app/components/Header'
+import React from 'react'
+
+type Props = {
+    params: {id: string},
+}
+
+export default function page({params}: Props) {
+  return (
+    <>
+        <Header />
+        <h2>Noticia {params.id}</h2>
+    </>
+  )
+}
