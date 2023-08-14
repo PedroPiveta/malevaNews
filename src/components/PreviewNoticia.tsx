09e8835e53f4b9f35df0a3f7d7db4982dcc5eefe
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-08-14 19:18:50 -0300

    added some styles

--- src/components/PreviewNoticia.tsx
+++ src/components/PreviewNoticia.tsx
@@ -1,16 +1,15 @@
 type newsProps = {
-  title: string,
-  body: string,
-  bannerUrl: string,
-}
+  title: string;
+  body: string;
+  bannerUrl: string;
+};
 
 export default function PreviewNoticia({ title, body, bannerUrl }: newsProps) {
-  
   return (
     <div>
-      <h1 className="text-white">{ title }</h1>
-      <p className="text-white">{ body }</p>
+      <h1 className="text-white">{title}</h1>
+      <p className="text-white">{body}</p>
       <img className="w-full" src={bannerUrl} alt="Banner" />
     </div>
-  )
+  );
 }
